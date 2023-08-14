2b3a46db3b1a2604d35d309387c2d48a1d27c006
Tracy-maker <ydlvns@gmail.com>
2023-08-14 17:49:42 +1000

    fix details of current weather

--- .history/src/app/Current/components/Metas/Metas_20230814173433.jsx
+++ .history/src/app/Current/components/Metas/Metas_20230814173433.jsx
@@ -3,9 +3,9 @@ import Meta from "./components/Meta/Meta";
 const Metas = ({data}) => {
   return (
     <div className="flex justify-center mt-4 text-center">
-      <Meta title="HUMIDITY">{data.main.humidity} %</Meta>
-      <div className="mx-8 border-2 border-white opacity-60"></div>
-      <Meta title="WIND">{data.wind.speed} m/s</Meta>
+      <Meta title="HUMIDITY">{data.main.humidity}</Meta>
+      <div className="mx-8 border-4 border-white opacity-60"></div>
+      <Meta title="WIND">1.34 K/M</Meta>
     </div>
   );
 };
