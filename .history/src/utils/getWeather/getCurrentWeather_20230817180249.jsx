1bb06f03ce04ca31188a93c5849831db9b70f522
Tracy-maker <ydlvns@gmail.com>
2023-08-17 18:15:05 +1000

    fix

---
+++ .history/src/utils/getWeather/getCurrentWeather_20230817180249.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { OPEN_WEATHER_MAP_BASE_URL, APP_ID } from "../../config";
+
+const getCurrentWeather = async (lat, lon) => {
+  try {
+    const response = await axios.get(`${OPEN_WEATHER_MAP_BASE_URL}/weather`, {
+      params: {
+        lat: lat,
+        lon: lon,
+        appid: APP_ID,
+        units: "metric",
+      },
+    });
+    if (!response.data) {
+      throw new Error("Response data is empty");
+    }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getCurrentWeather;
